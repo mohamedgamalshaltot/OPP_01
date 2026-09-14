fa25c3a85c4c9c6dfc13408c51580960b167b70f
mohamedgamalshaltot <mohmedgamalshaltot@gmail.com>
2026-09-14 17:21:03 +0300

    Question 04 in session 05

---
+++ OPP_01/Genre.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPP_01
+{
+    internal enum Genre
+    {
+        Fiction,
+        NonFiction,
+        Science
+    }
+}
--- OPP_01/Program.cs
+++ OPP_01/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace OPP_01
+﻿using System.Drawing;
+
+namespace OPP_01
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Question 04 in session 05
+            //4-Declare an enum Genre { Fiction, NonFiction, Science }. Add a Genre
+            //property to Book, assign it Genre.Science, and print it.
+            Book myBook = new Book();
+            myBook.BookGenre = Genre.Science;
+            Console.WriteLine(myBook.BookGenre);
+            #endregion
         }
     }
 }
